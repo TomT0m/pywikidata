413963a39b56ccfc09be9bc1ae89d1a80376578c
Thomas Douillard <thomas.douillard@gmail.com>
2013-04-18 21:01:35 +0200

    Config management :
    ability to have a config file in xdg config path "/wp/config.py" ; eg.
    ${XDG_CONFIG_HOME}/wp/config.py

--- wikidata.py
+++ wikidata.py
@@ -6,9 +6,20 @@ from configReader import Config
 from api import API
 
 import errors
+import xdg.BaseDirectory
 
-config = Config('config.py')
+import os
+
+configfile_path = xdg.BaseDirectory.load_first_config("wp/config.py")
+
+if (os.path.exists(configfile_path)):
+	config = Config(configfile_path)
+else:
+	config = Config('config.py')
+
+
 if not config["api"]:
-    raise errors.ConfigurationError("An API url needs to be defined in config.py")
+	raise errors.ConfigurationError("An API url needs to be defined in config.py")
 
 api = API(config)
+
